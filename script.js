b9264de4f61b1ce14df51a03d5b1e834dd34a6e5
sharlyl <sharlylouisephine@gmail.com>
2021-11-11 11:15:42 +0700

    Some revisions

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function updateRate(){
 
 function compute(){
     var principal = document.getElementById("principal").value;
-
+    
     if (principal <=0){
         alert("Enter a positive number")
     } else{
@@ -19,6 +19,17 @@ function compute(){
         \<br>You will receive an amount of <mark>"+interest+"</mark>,\
         \<br>in the year <mark>"+year+"</mark>\
         \</p>";
+
+        var target_interest = document.getElementById("target_interest").value;
+        if(target_interest!=""){
+            if(interest<target_interest){
+                document.getElementById("recommendation").innerHTML="<p>To meet your tagret, you need to \<br> increase the \
+                <mark>Amount</mark> invested, the Interest <mark>Rate</mark>, \
+                \<br>or <mark>No. of Years</mark></p>"
+            }else{
+                document.getElementById("recommendation").innerHTML="<p>You will achieve your target</p>"
+            }
+        }
     }
 }
 
